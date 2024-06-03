86dbf338e373f7d22884f0a9ca116c31c08fae2a
Edwan27 <edwan@student.untan.ac.id>
2024-06-03 22:48:38 +0700

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables
+
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Dashboard",
+          style: TextStyle(backgroundColor: Colors.amberAccent),
+        ),
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Text(
+                "Dashboard",
+                style: TextStyle(backgroundColor: Colors.amberAccent),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
